433ad75bec1f349a4bc522b1321b2ced33a394e9
Eng Ibrahim Elshishtawy <190879691+IbrahimElshishtawy@users.noreply.github.com>
2026-06-19 17:13:45 +0300

    feat: initialize Isar database schema collection file

--- hoshha/lib/infrastructure/database/app_database_schemas.dart
+++ hoshha/lib/infrastructure/database/app_database_schemas.dart
@@ -1,15 +1,3 @@
 import 'package:isar/isar.dart';
 
-import '../../features/budgets/data/local/budget_record.dart';
-import '../../features/expenses/data/local/expense_record.dart';
-import '../../features/gamification/data/local/achievement_progress_record.dart';
-import '../../features/gamification/data/local/streak_record.dart';
-import '../../features/gamification/data/local/user_stats_record.dart';
-
-final List<IsarGeneratedSchema> appDatabaseSchemas = [
-  ExpenseRecordSchema,
-  BudgetRecordSchema,
-  UserStatsRecordSchema,
-  StreakRecordSchema,
-  AchievementProgressRecordSchema,
-];
+final List<IsarGeneratedSchema> appDatabaseSchemas = [];
