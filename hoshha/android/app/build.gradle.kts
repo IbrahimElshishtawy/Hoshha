dae87055f74677bf98a58c1c9e551f6f07ae56a4
Eng Ibrahim Elshishtawy <190879691+IbrahimElshishtawy@users.noreply.github.com>
2026-06-25 21:02:38 +0300

    build: migrate android app build configuration to build.gradle.kts

--- hoshha/android/app/build.gradle.kts
+++ hoshha/android/app/build.gradle.kts
@@ -7,7 +7,13 @@ plugins {
 android {
     namespace = "com.example.hoshha"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "28.2.13676358"
+
+    externalNativeBuild {
+        cmake {
+            version = "4.0.2"
+        }
+    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
