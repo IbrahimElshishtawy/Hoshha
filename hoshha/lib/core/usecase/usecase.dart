34818824ae0320c805a02b7bdcbdb7387be445b8
Eng Ibrahim Elshishtawy <190879691+IbrahimElshishtawy@users.noreply.github.com>
2026-06-30 18:17:42 +0300

    feat: define generic UseCase base class and NoParams implementation

--- hoshha/lib/core/usecase/usecase.dart
+++ hoshha/lib/core/usecase/usecase.dart
@@ -2,8 +2,8 @@ import 'package:equatable/equatable.dart';
 import '../error/failure.dart';
 import '../error/result.dart';
 
-abstract class UseCase<Type, Params> {
-  Future<Result<Type, Failure>> call(Params params);
+abstract class UseCase<TypeResult, Params> {
+  Future<Result<TypeResult, Failure>> call(Params params);
 }
 
 class NoParams extends Equatable {
